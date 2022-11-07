63c9e351c25b1e616df05433229f284b5e5e506b
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2022-11-07 09:18:21 -0300

    alteração de segurança

--- src/br/com/saude/service/EmailService.java
+++ src/br/com/saude/service/EmailService.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 public class EmailService {
 
     private final String username = "santos852.rafael@gmail.com";
-    private final String password = "hbkcwrdbabrhvhsx";
+    private final String password = "senhaApp";
 
     public void enviarEmail(String email) {
         Properties prop = new Properties();
